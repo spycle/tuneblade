acda7a22e51b6f05481aea08042757d5418347c4
spycle <48740594+spycle@users.noreply.github.com>
2021-12-10 22:13:22 +0000

    Add files via upload

---
+++ custom_components/tuneblade/media_player.py
@@ -0,0 +1,78 @@
+"""Media Player platform for TuneBlade."""
+from homeassistant.components.media_player import MediaPlayerEntity
+from homeassistant.components.media_player.const import (
+    SUPPORT_VOLUME_SET,
+    SUPPORT_TURN_ON,
+    SUPPORT_TURN_OFF
+)
+from homeassistant.const import (
+    STATE_OFF,
+    STATE_PLAYING
+    )
+from .const import DEFAULT_NAME, DOMAIN, ICON, MEDIA_PLAYER
+from .entity import TuneBladeEntity
+
+SUPPORTED_FEATURES = (
+    SUPPORT_VOLUME_SET
+    | SUPPORT_TURN_ON
+    | SUPPORT_TURN_OFF
+)
+
+async def async_setup_entry(hass, entry, async_add_devices):
+    """Setup sensor platform."""
+    coordinator = hass.data[DOMAIN][entry.entry_id]
+    async_add_devices([TuneBladeMediaPlayer(coordinator, entry)])
+
+
+class TuneBladeMediaPlayer(TuneBladeEntity, MediaPlayerEntity):
+    """TuneBlade Media Player class."""
+
+    async def async_turn_on(self, **kwargs):  # pylint: disable=unused-argument
+        """Turn on the switch."""
+        await self.coordinator.api.async_conn("Connect")
+        await self.coordinator.async_request_refresh()
+
+    async def async_turn_off(self, **kwargs):  # pylint: disable=unused-argument
+        """Turn off the switch."""
+        await self.coordinator.api.async_conn("Disconnect")
+        await self.coordinator.async_request_refresh()
+
+    async def async_set_volume_level(self, volume):
+        """Set volume level, range 0..1."""
+        await self.coordinator.api.async_set_volume(volume)
+        await self.coordinator.async_request_refresh()
+
+    @property
+    def name(self):
+        """Return the name of the switch."""
+        device = self.coordinator.data.get("Name")
+        name = device+" "+NAME
+        return name
+
+    @property
+    def volume_level(self):
+        """Volume level of the media player (0..1)."""
+        volume = self.coordinator.data.get("Volume", "")
+        return volume / 100.0
+
+    @property
+    def icon(self):
+        """Return the icon of this switch."""
+        return ICON
+
+    @property
+    def is_on(self):
+        """Return true if the switch is on."""
+        return self.coordinator.data.get("Status", "") in ['Connected','Connecting']
+
+    @property
+    def state(self):
+        if self.coordinator.data.get("Status", "") in ['Connected','Connecting']:
+            return STATE_PLAYING
+        else:
+            return STATE_OFF
+
+    @property
+    def supported_features(self):
+        """Flag media player features that are supported."""
+        return SUPPORTED_FEATURES
---
+++ custom_components/tuneblade/tuneblade.py
@@ -0,0 +1,83 @@
+"""TuneBlade API Client."""
+import logging
+import asyncio
+import socket
+from typing import Optional
+import aiohttp
+import async_timeout
+
+TIMEOUT = 10
+
+
+_LOGGER: logging.Logger = logging.getLogger(__package__)
+
+HEADERS = {"Content-type": "application/json; charset=UTF-8"}
+
+class TuneBladeApiClient:
+    def __init__(
+        self, host: str, port: str, device_id: str, username: str, password: str, airplay_password: str, session: aiohttp.ClientSession, auth
+    ) -> None:
+        """Sample API Client."""
+        self._host = host
+        self._port = port
+        self._device_id = device_id
+        self._username = username
+        self._password = password
+        self._airplay_password = airplay_password
+        self._session = session
+        self._url = "http://"+host+":"+port+"/devices/"+device_id
+        self._auth = auth
+        self._volume = 100
+
+    async def async_get_data(self) -> dict:
+        """Get data from the API."""
+        return await self.api_wrapper("get", self._url)
+
+    async def async_conn(self, value: str) -> None:
+        """Get data from the API."""
+        await self.api_wrapper("put", self._url, data={"Password": self._airplay_password, "Status": value}, headers=HEADERS)
+
+    async def async_set_volume(self, volume: str) -> None:
+        """Get data from the API."""
+        await self.api_wrapper("put", self._url, data={"Password": self._airplay_password, "Volume": str(int(volume*100))}, headers=HEADERS)
+
+    async def api_wrapper(
+        self, method: str, url: str, data: dict = {}, headers: dict = {}
+    ) -> dict:
+        """Get information from the API."""
+        try:
+            async with async_timeout.timeout(TIMEOUT):
+                if method == "get":
+                    response = await self._session.get(url, headers=headers)
+                    return await response.json()
+
+                elif method == "put":
+                    await self._session.put(url, headers=headers, json=data)
+
+                elif method == "patch":
+                    await self._session.patch(url, headers=headers, json=data)
+
+                elif method == "post":
+                    await self._session.post(url, headers=headers, json=data)
+
+        except asyncio.TimeoutError as exception:
+            _LOGGER.error(
+                "Timeout error fetching information from %s - %s",
+                url,
+                exception,
+            )
+
+        except (KeyError, TypeError) as exception:
+            _LOGGER.error(
+                "Error parsing information from %s - %s",
+                url,
+                exception,
+            )
+        except (aiohttp.ClientError, socket.gaierror) as exception:
+            _LOGGER.error(
+                "Error fetching information from %s - %s",
+                url,
+                exception,
+            )
+        except Exception as exception:  # pylint: disable=broad-except
+            _LOGGER.error("Something really wrong happened! - %s", exception)
