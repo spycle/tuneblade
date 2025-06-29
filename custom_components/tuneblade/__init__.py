b6d84ddf56a4695ac61adaa5b261f1debbf2529b
MattMorgan <48740594+spycle@users.noreply.github.com>
2025-06-29 14:05:37 +0100

    Update __init__.py

--- custom_components/tuneblade/__init__.py
+++ custom_components/tuneblade/__init__.py
@@ -11,6 +11,7 @@ from homeassistant.core import Config, HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+from homeassistant.helpers import config_validation as cv
 
 from .tuneblade import TuneBladeApiClient
 
@@ -25,6 +26,8 @@ from .const import (
     PLATFORMS,
     STARTUP_MESSAGE,
 )
+
+CONFIG_SCHEMA = cv.config_entry_only_config_schema("tuneblade")
 
 SCAN_INTERVAL = timedelta(seconds=30)
 
