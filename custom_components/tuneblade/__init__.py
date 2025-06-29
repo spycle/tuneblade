a3869efaa3864eb42a9adb435c197026662df597
MattMorgan <48740594+spycle@users.noreply.github.com>
2025-06-29 13:28:21 +0100

    Update __init__.py

--- custom_components/tuneblade/__init__.py
+++ custom_components/tuneblade/__init__.py
@@ -68,8 +68,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     for platform in PLATFORMS:
         if entry.options.get(platform, True):
             coordinator.platforms.append(platform)
-            hass.async_add_job(
-                hass.config_entries.async_forward_entry_setup(entry, platform)
+            hass.async_create_background_task(
+                hass.config_entries.async_forward_entry_setups(entry, [platform]),
+                name=f"tuneblade: forward {platform}"
             )
 
     entry.add_update_listener(async_reload_entry)
